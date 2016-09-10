48b4b5f4b2d9f266298bf796fbca226a1e870b78
Iker Zamora <ikerzamora94@gmail.com>
2016-09-10 23:49:27 +0200

    Fix PEP8 errors

--- encounters.py
+++ encounters.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime, timedelta
+from datetime import datetime
 
 EE = 'EE'
 AE = 'AE'
 GE = 'GE'
 
+
 class Encounter:
 
-	def __init__(self, date, acronym, edition):
-		self.date = date
-		self.acronym = acronym
-		self.edition = edition
+    def __init__(self, date, acronym, edition):
+        self.date = date
+        self.acronym = acronym
+        self.edition = edition
 
-	def time_left(self):
-		'''
-		Return: days, hours, minutes and seconds remaining for the given date
-		'''
-		now = datetime.now()
-		td = self.date - now
-		return td.days, td.seconds//3600, (td.seconds//60)%60, (td.seconds%3600)%60
+    def time_left(self):
+        '''
+        Return: days, hours, minutes and seconds
+                remaining for the next LAN party
+        '''
+        now = datetime.now()
+        td = self.date - now
+        days = td.days
+        hours = td.seconds // 3600
+        minutes = (td.seconds // 60) % 60
+        seconds = (td.seconds % 3600) % 60
+        return days, hours, minutes, seconds
